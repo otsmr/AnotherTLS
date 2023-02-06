c7a21cd45e1c13232357cce083137b627bb9bee8
otsmr <git@tsmr.eu>
2023-02-06 23:34:52 +0100

    update

--- src/bin/https.rs
+++ src/bin/https.rs
@@ -1,4 +1,4 @@
-use anothertls::{update_to_tls_listener, TlsConfig, TlsListener};
+use anothertls::{TlsConfig, TlsListener};
 use std::{
     io,
     net::{TcpListener, ToSocketAddrs},
@@ -12,18 +12,40 @@ impl HttpsServer {
     pub fn bind<A: ToSocketAddrs>(addr: A) -> io::Result<Self> {
         let listener = TcpListener::bind(addr)?;
 
-        let listener = update_to_tls_listener(listener, TlsConfig {});
+        let listener = TlsListener::new(listener, TlsConfig {});
 
         Ok(Self { listener })
     }
 
     pub fn static_file_server(&self, _http_root: &str) {
-        match self.listener.accept() {
-            Ok((_socket, _addr)) => {
-                log::debug!("New Connection");
-            }
-            Err(e) => log::error!("couldn't get client: {:?}", e),
-        };
+        loop {
+            match self.listener.accept() {
+                Ok((mut socket, _addr)) => {
+
+                    if socket.connect_block().is_err() {
+                        break;
+                    }
+
+                    log::debug!("New Connection");
+
+                    let mut buf: [u8; 4096] = [0; 4096];
+
+                    loop {
+
+                        let n = match socket.read(&mut buf) {
+                            Ok(n) => n,
+                            _ => break,
+                        };
+
+                        log::info!("Read from socket: {}", n);
+
+                    }
+
+
+                }
+                Err(e) => log::error!("couldn't get client: {:?}", e),
+            };
+        }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 use std::{
-    io,
-    net::{SocketAddr, TcpListener, TcpStream},
+    io::{self, Read},
+    net::{SocketAddr, TcpStream, TcpListener}, os::raw,
 };
+use std::io::Result;
+
+
+#[derive(PartialEq)]
+enum HandshakeState {
+    WaitingForClientHello,
+    SendServerHello,
+    Finished,
+}
 
 pub struct TlsStream<'a> {
     stream: TcpStream,
@@ -9,10 +18,83 @@ pub struct TlsStream<'a> {
     config: &'a TlsConfig
 }
 
-impl<'a> TlsStream<'a> {
-    pub fn new(stream: TcpStream, addr: SocketAddr, config: &'a TlsConfig) -> Self {
-        Self { stream, addr, config }
+pub struct Extension {
+
+}
+
+pub struct CipherSuite {
+
+}
+
+struct ClientHello {
+    legacy_version: u16,
+    random: [u8; 32],
+    // legacy_session_id,
+    cipher_suites: [CipherSuite; 200], // FIXME: 2**16-2
+    legacy_compression_methods: [u8; 32],
+    extensions: Extension,
+}
+impl ClientHello {
+    pub fn from_raw(buf: &[u8]) {
+
+
+
     }
+}
+
+impl<'a> TlsStream<'a> {
+
+    pub fn new(stream: TcpStream, addr: SocketAddr, config: &'a TlsConfig) -> Self {
+        Self { stream,  addr, config }
+    }
+
+
+    pub fn connect_block(&mut self) -> Result<()> {
+
+        let mut state = HandshakeState::WaitingForClientHello;
+
+        while state != HandshakeState::Finished {
+
+            let mut raw_buf: [u8; 4096] = [0; 4096];
+
+            let n = self.stream.read(&mut raw_buf)?;
+
+
+            println!("{:?}", &raw_buf[..n]);
+
+            let client_hello = ClientHello::from_raw(&raw_buf[..n]);
+
+            // println!("{:?}", client_hello);
+
+
+
+            panic!("");
+
+
+            state = HandshakeState::Finished;
+
+        }
+
+        Ok(())
+
+
+    }
+
+    pub fn read<'b>(&'b mut self, buf: &'b mut [u8]) -> Result<usize> {
+
+        let mut raw_buf: [u8; 4096] = [0; 4096];
+
+        let n = self.stream.read(&mut raw_buf)?;
+
+
+        Ok(n)
+
+    }
+
+    pub fn write<'b>(&'b mut self, src: &'b [u8]) {
+
+    }
+
 }
 
 pub struct TlsConfig {
@@ -25,12 +107,9 @@ pub struct TlsListener {
 }
 
 impl TlsListener {
+
     pub fn new(server: TcpListener, config: TlsConfig) -> Self {
         Self { server, config }
-    }
-
-    pub fn local_addr(&self) -> io::Result<SocketAddr> {
-        self.server.local_addr()
     }
 
     pub fn accept(&self) -> io::Result<(TlsStream, SocketAddr)> {
@@ -41,9 +120,5 @@ impl TlsListener {
 
         Ok((stream, addr))
     }
-}
 
-pub fn update_to_tls_listener(tcp: TcpListener, config: TlsConfig) -> TlsListener {
-    TlsListener::new(tcp, config)
 }
-
